948a74445c373563d728a707db8bdeb5a72efe9d
cloud-ima <cloud@municipalidadarica.cl>
2026-01-14 10:45:29 -0300

    feat: correción del modulo de usuarios

--- directores/formulario.php
+++ directores/formulario.php
@@ -1,37 +1,60 @@
 <?php
 
 include("../conexion.php");
+include("../debug.php");
 
-$x_flag = $_GET["flag"] ?? '';
+// Inicializar variables - directores usa: nom, tit, car, codigo
+$parz = 1;
+$nomz = "";
+$titz = "";
+$carz = "";
+$idz = "";
+$idestado = "Estado(Ingresando Nuevo Registro)";
 
-if ( $x_flag == 0 ) {
+$x_flag = $_GET["flag"] ?? "";
+$x_param = $_GET["param"] ?? "";
+
+debug_log("=== FORMULARIO DIRECTORES ===");
+debug_log("flag", $x_flag);
+debug_log("param", $x_param);
+
+// Si viene param=1 desde el index o no hay flag, es nuevo registro
+if ( $x_param == 1 || $x_flag == "" || $x_flag == 0 ) {
    $idestado = "Estado(Ingresando Nuevo Registro)";
    $parz = 1;
    $nomz = "";
-}   
+   $titz = "";
+   $carz = "";
+   $idz = "";
+   debug_log("Modo: NUEVO REGISTRO, parz=", $parz);
+}
 
 if ( $x_flag == 1 ) {
-    $idestado = "Estado(Modificación del Registro)";
-	$link=conectarse();
-    $idz=$_GET['id'] ?? '';
+    $idestado = "Estado(Modificacion del Registro)";
+    $link=conectarse();
+    $idz=$_GET["id"] ?? "";
     $ssql = "select * from directores where id ='$idz'";
-    $rs = mysql_query($ssql,$link); 
-    $num_registros = mysql_num_rows($rs); 
-	if ($num_registros == 0){
-		   header("location: index.php");
-	}else{
-		   while ($row = mysql_fetch_array($rs)){
-   				  $nomz = $row["nombre"];
-   				  $titz = $row["titulo"];
-   				  $carz = $row["cargo"];				  				  
-				  $idz = $row["id"];
-				  $parz = 2;
-          }
-	}
-				  mysql_close($link);
+    debug_log("SQL edicion", $ssql);
+    $rs = mysql_query($ssql,$link);
+    $num_registros = mysql_num_rows($rs);
+    debug_log("Registros encontrados", $num_registros);
+    if ($num_registros == 0){
+        header("location: index.php");
+    }else{
+        while ($row = mysql_fetch_array($rs)){
+            $nomz = $row["nombre"];
+            $titz = $row["titulo"];
+            $carz = $row["cargo"];
+            $idz = $row["id"];
+            $parz = 2;
+            debug_log("Datos cargados", ["nombre"=>$nomz, "titulo"=>$titz, "cargo"=>$carz]);
+        }
+    }
+    mysql_close($link);
+    debug_log("Modo: EDICION, parz=", $parz);
 }
-//include "../seguridad.php";
-$fechaactualed = date('d')."/".date('n')."/".date('Y');
+
+$fechaactualed = date("d")."/".date("n")."/".date("Y");
 ?>
 <html>
 <head>
@@ -40,15 +63,15 @@ $fechaactualed = date('d')."/".date('n')."/".date('Y');
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 </head>
 <body>
-<form action="mantenedor.php" method="POST" enctype="multipart/form-data" >
+<form action="mantenedor.php" method="POST" enctype="multipart/form-data">
   <br>
   <br>
   <table width="467" border="0" align="center" cellpadding="0" cellspacing="0">
     <tr>
-      <td width="467" height="200" valign="top"> 
+      <td width="467" height="200" valign="top">
         <table width="461" border="0" cellpadding="2" cellspacing="0">
-          <tr> 
-            <td width="461" align="center"> <div align="left"><strong>Usos de Suelo <font size="2"></font></strong></div></td>
+          <tr>
+            <td width="461" align="center"><div align="left"><strong>Directores</strong></div></td>
           </tr>
           <tr>
             <td align="center"><div align="left"><?php echo $idestado ?></div></td>
@@ -56,61 +79,61 @@ $fechaactualed = date('d')."/".date('n')."/".date('Y');
         </table>
         <table width="459" border="0" cellpadding="0" cellspacing="0" bordercolor="#cccccc">
           <tr>
-            <td width="616" height="68"> 
+            <td width="616" height="68">
               <table width="454" border="0" cellpadding="2" cellspacing="2">
-                <!-- <tr> -->
-                <!-- <td class=estilo_titulo >Código</td><td> -->
-                <input type=hidden  class=estilo_text  name=t08_cod value=''>
-                <!-- </td> -->
-                <!-- </tr> -->
-                <tr> 
-                  <td width="146" height="26" bgcolor="#efefef" class=estilo_titulo >Nombre</td>
-                  <td width="294" bgcolor="#efefef"> 
-                    <input name="nom" type="text" id="titulo2" value="<?php echo $nomz ?>" size="50" maxlength="100">
+                <tr>
+                  <td width="146" height="26" bgcolor="#efefef" class="estilo_titulo">Nombre</td>
+                  <td width="294" bgcolor="#efefef">
+                    <input name="nom" type="text" value="<?php echo $nomz ?>" size="50" maxlength="100">
                   </td>
                 </tr>
                 <tr>
-                  <td height="19" bgcolor="#efefef" class=estilo_titulo >T&iacute;tulo</td>
-                  <td bgcolor="#FFFFFF"><input name="tit" type="text" id="titulo2" value="<?php echo $titz ?>" size="50" maxlength="100"></td>
+                  <td height="19" bgcolor="#efefef" class="estilo_titulo">Titulo</td>
+                  <td bgcolor="#FFFFFF">
+                    <input name="tit" type="text" value="<?php echo $titz ?>" size="50" maxlength="100">
+                  </td>
                 </tr>
                 <tr>
-                  <td height="19" bgcolor="#efefef" class=estilo_titulo >Cargo</td>
-                  <td bgcolor="#FFFFFF"><input name="car" type="text" id="titulo2" value="<?php echo $carz ?>" size="50" maxlength="100"></td>
+                  <td height="19" bgcolor="#efefef" class="estilo_titulo">Cargo</td>
+                  <td bgcolor="#FFFFFF">
+                    <input name="car" type="text" value="<?php echo $carz ?>" size="50" maxlength="100">
+                  </td>
                 </tr>
-                <tr> 
-                  <td height="19" class=estilo_titulo > <input name="param" type="hidden" id="param4" value="<?php echo $parz ?>"> 
-                    <input name="codigo" type="hidden" id="param5" value="<?php echo $idz ?>"></td>
+                <tr>
+                  <td height="19" class="estilo_titulo">
+                    <input name="param" type="hidden" value="<?php echo $parz ?>">
+                    <input name="codigo" type="hidden" value="<?php echo $idz ?>">
+                  </td>
                   <td bgcolor="#FFFFFF">&nbsp;</td>
                 </tr>
-              </table></td>
+              </table>
+            </td>
           </tr>
         </table>
         <table width="454" border="0" cellpadding="0" cellspacing="0">
-          <!-- <tr> -->
-          <!-- <td class=estilo_titulo >Código</td><td> -->
-          <input type=hidden  class=estilo_text  name=t08_cod2 value=''>
-          <!-- </td> -->
-          <!-- </tr> -->
-          <tr> 
-            <td height="39" class=estilo_titulo > 
+          <tr>
+            <td height="39" class="estilo_titulo">
               <table width="178" border="0" align="left">
-                <tr> 
-                  <td width="73" valign="top"> <div align="center">
+                <tr>
+                  <td width="73" valign="top">
+                    <div align="center">
                       <input name="enviar" type="submit" id="enviar" value="Grabar">
-                    </div></td>
+                    </div>
+                  </td>
                   <td width="95" valign="top">
-<input type="reset" name="Submit3" value="Cancelar" onClick="cerrar()">
-				  <script language="javascript">
-function cerrar()
-{
-	opener.window.location.reload( false );
-	window.close();
-}
-  </script>
+                    <input type="reset" name="Submit3" value="Cancelar" onClick="cerrar()">
+                    <script language="javascript">
+                      function cerrar() {
+                        opener.window.location.reload(false);
+                        window.close();
+                      }
+                    </script>
+                  </td>
                 </tr>
-              </table></td>
+              </table>
+            </td>
           </tr>
-        </table> 
+        </table>
       </td>
     </tr>
   </table>
--- usuarios/formulario.php
+++ usuarios/formulario.php
@@ -1,38 +1,63 @@
 <?php
 
 include("../conexion.php");
+include("../debug.php");
 
-$x_flag = $_GET["flag"] ?? '';
+// Inicializar variables - usuarios usa: nombre, cuenta, pass, tipo, codigo
+$parz = 1;
+$nomz = "";
+$cuentaz = "";
+$tipoz = "";
+$passz = "";
+$idz = "";
+$idestado = "Estado(Ingresando Nuevo Registro)";
 
-if ( $x_flag == 0 ) {
+$x_flag = $_GET["flag"] ?? "";
+$x_param = $_GET["param"] ?? "";
+
+debug_log("=== FORMULARIO USUARIOS ===");
+debug_log("flag", $x_flag);
+debug_log("param", $x_param);
+
+// Si viene param=1 desde el index o no hay flag, es nuevo registro
+if ( $x_param == 1 || $x_flag == "" || $x_flag == 0 ) {
    $idestado = "Estado(Ingresando Nuevo Registro)";
    $parz = 1;
    $nomz = "";
-}   
+   $cuentaz = "";
+   $tipoz = "";
+   $passz = "";
+   $idz = "";
+   debug_log("Modo: NUEVO REGISTRO, parz=", $parz);
+}
 
 if ( $x_flag == 1 ) {
-    $idestado = "Estado(Modificación del Registro)";
-	$link=conectarse();
-    $idz=$_GET['id'] ?? '';
+    $idestado = "Estado(Modificacion del Registro)";
+    $link=conectarse();
+    $idz=$_GET["id"] ?? "";
     $ssql = "select * from usuarios where id ='$idz'";
+    debug_log("SQL edicion", $ssql);
     $rs = mysql_query($ssql,$link);
-    $num_registros = mysql_num_rows($rs); 
-	if ($num_registros == 0){
-		   header("location: index.php");
-	}else{
-		   while ($row = mysql_fetch_array($rs)){
-   				  $nomz = $row["nombre"];
-   				  $cuentaz = $row["usuario"];
-   				  $tipoz = $row["tipo"];
-   				  $passz = $row["password"];
-				  $idz = $row["id"];
-				  $parz = 2;
-          }
-	}
-				  mysql_close($link);
+    $num_registros = mysql_num_rows($rs);
+    debug_log("Registros encontrados", $num_registros);
+    if ($num_registros == 0){
+        header("location: index.php");
+    }else{
+        while ($row = mysql_fetch_array($rs)){
+            $nomz = $row["nombre"];
+            $cuentaz = $row["usuario"];
+            $tipoz = $row["tipo"];
+            $passz = $row["password"];
+            $idz = $row["id"];
+            $parz = 2;
+            debug_log("Datos cargados", ["nombre"=>$nomz, "cuenta"=>$cuentaz, "tipo"=>$tipoz]);
+        }
+    }
+    mysql_close($link);
+    debug_log("Modo: EDICION, parz=", $parz);
 }
-//include "seguridad.php";
-$fechaactualed = date('d')."/".date('n')."/".date('Y');
+
+$fechaactualed = date("d")."/".date("n")."/".date("Y");
 ?>
 <html>
 <head>
@@ -41,15 +66,13 @@ $fechaactualed = date('d')."/".date('n')."/".date('Y');
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 </head>
 <body>
-<form action="mantenedor.php" method="POST" enctype="multipart/form-data" >
+<form action="mantenedor.php" method="POST" enctype="multipart/form-data">
   <table width="467" border="0" align="center" cellpadding="0" cellspacing="0">
     <tr>
-      <td width="467" height="200" valign="top"> 
+      <td width="467" height="200" valign="top">
         <table width="461" border="0" cellpadding="2" cellspacing="0">
-          <tr> 
-            <td width="461" align="center"> <div align="left"><strong><br>
-              Creaci&oacute;n 
-                de Usuarios<font size="2"></font></strong></div></td>
+          <tr>
+            <td width="461" align="center"><div align="left"><strong><br>Creacion de Usuarios</strong></div></td>
           </tr>
           <tr>
             <td align="center"><div align="left"><?php echo $idestado ?></div></td>
@@ -57,82 +80,84 @@ $fechaactualed = date('d')."/".date('n')."/".date('Y');
         </table>
         <table width="459" border="0" cellpadding="0" cellspacing="0" bordercolor="#cccccc">
           <tr>
-            <td width="616" height="68"> 
+            <td width="616" height="68">
               <table width="454" border="0" cellpadding="2" cellspacing="2">
-                <!-- <tr> -->
-                <!-- <td class=estilo_titulo >Código</td><td> -->
-                <input type=hidden  class=estilo_text  name=t08_cod value=''>
-                <!-- </td> -->
-                <!-- </tr> -->
                 <tr>
-                  <td height="26" bgcolor="#efefef" class=estilo_titulo >Cuenta</td>
-                  <td bgcolor="#efefef"><input name="cuenta" type="text" id="nombre2" value="<?php echo $cuentaz ?>" size="20" maxlength="100"></td>
-                </tr>
-                <tr> 
-                  <td width="146" height="26" bgcolor="#efefef" class=estilo_titulo >Nombre del usuario </td>
-                  <td width="294" bgcolor="#efefef"> <input name="nombre" type="text" id="titulo2" value="<?php echo $nomz ?>" size="48" maxlength="100"> 
+                  <td height="26" bgcolor="#efefef" class="estilo_titulo">Cuenta</td>
+                  <td bgcolor="#efefef">
+                    <input name="cuenta" type="text" value="<?php echo $cuentaz ?>" size="20" maxlength="100">
                   </td>
                 </tr>
-                <tr> 
-                  <td height="19" bgcolor="#efefef" class=estilo_titulo >Tipo usuario </td>
-                  <td bgcolor="#efefef"> 
+                <tr>
+                  <td width="146" height="26" bgcolor="#efefef" class="estilo_titulo">Nombre del usuario</td>
+                  <td width="294" bgcolor="#efefef">
+                    <input name="nombre" type="text" value="<?php echo $nomz ?>" size="48" maxlength="100">
+                  </td>
+                </tr>
+                <tr>
+                  <td height="19" bgcolor="#efefef" class="estilo_titulo">Tipo usuario</td>
+                  <td bgcolor="#efefef">
                     <?php
-$linkc=conectarse();
-$resultc = mysql_query("SELECT * FROM tipousuarios order by id",$linkc);
-?>
-                    <select class=bordecampos name="tipo" id="tipo">
+                    $linkc=conectarse();
+                    $resultc = mysql_query("SELECT * FROM tipousuarios order by id",$linkc);
+                    ?>
+                    <select class="bordecampos" name="tipo" id="tipo">
                       <?php
-while($rowc = mysql_fetch_array($resultc)) {
-?>
-                      <option value="<?php echo $rowc["id"] ?>"
-<?php if($rowc["id"] == $tipoz){?>selected<?php }?>> <?php echo $rowc["nombre"]?> </option>
+                      while($rowc = mysql_fetch_array($resultc)) {
+                      ?>
+                      <option value="<?php echo $rowc["id"] ?>" <?php if($rowc["id"] == $tipoz){ echo "selected"; }?>>
+                        <?php echo $rowc["nombre"]?>
+                      </option>
                       <?php }
-mysql_close($linkc);
-?>
-                    </select> </td>
-                </tr>
-                <tr> 
-                  <td height="19" bgcolor="#efefef" class=estilo_titulo >Password</td>
-                  <td bgcolor="#efefef"> <input name="pass" type="password" id="nombre2" value="<?php echo $passz ?>" size="20" maxlength="100"> 
+                      mysql_close($linkc);
+                      ?>
+                    </select>
                   </td>
                 </tr>
-                <tr> 
-                  <td height="19" class=estilo_titulo > <input name="param" type="hidden" id="param4" value="<?php echo $parz ?>"> 
-                    <input name="codigo" type="hidden" id="param5" value="<?php echo $idz ?>"></td>
+                <tr>
+                  <td height="19" bgcolor="#efefef" class="estilo_titulo">Password</td>
+                  <td bgcolor="#efefef">
+                    <input name="pass" type="password" value="<?php echo $passz ?>" size="20" maxlength="100">
+                  </td>
+                </tr>
+                <tr>
+                  <td height="19" class="estilo_titulo">
+                    <input name="param" type="hidden" value="<?php echo $parz ?>">
+                    <input name="codigo" type="hidden" value="<?php echo $idz ?>">
+                  </td>
                   <td bgcolor="#FFFFFF">&nbsp;</td>
                 </tr>
-              </table></td>
+              </table>
+            </td>
           </tr>
         </table>
         <table width="454" border="0" cellpadding="0" cellspacing="0">
-          <!-- <tr> -->
-          <!-- <td class=estilo_titulo >Código</td><td> -->
-          <input type=hidden  class=estilo_text  name=t08_cod2 value=''>
-          <!-- </td> -->
-          <!-- </tr> -->
-          <tr> 
-            <td height="39" class=estilo_titulo > 
+          <tr>
+            <td height="39" class="estilo_titulo">
               <table width="178" border="0" align="left">
-                <tr> 
-                  <td width="73" valign="top"> <div align="center">
+                <tr>
+                  <td width="73" valign="top">
+                    <div align="center">
                       <input name="enviar" type="submit" id="enviar" value="Grabar">
-                    </div></td>
+                    </div>
+                  </td>
                   <td width="95" valign="top">
-<input type="reset" name="Submit3" value="Cancelar" onClick="cerrar()">
-				  <script language="javascript">
-function cerrar()
-{
-	opener.window.location.reload( false );
-	window.close();
-}
-  </script>
+                    <input type="reset" name="Submit3" value="Cancelar" onClick="cerrar()">
+                    <script language="javascript">
+                      function cerrar() {
+                        opener.window.location.reload(false);
+                        window.close();
+                      }
+                    </script>
+                  </td>
                 </tr>
-              </table></td>
+              </table>
+            </td>
           </tr>
-        </table> 
+        </table>
       </td>
     </tr>
   </table>
-  </form>
+</form>
 </body>
 </html>
