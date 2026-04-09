a071f10f874b90e003c33c781e079d3a4859bf9d
cloud-ima <cloud@municipalidadarica.cl>
2026-04-09 09:34:28 -0400

    fix correción de indices

--- fix_indexes.sql
+++ fix_indexes.sql
@@ -1,24 +1,23 @@
 -- ============================================================
 -- INDEXES CRITICOS para resolver CPU al 100%
 -- Ejecutar en el servidor Ubuntu: mysql -u root -p domsistemas < fix_indexes.sql
+-- Este script se puede ejecutar multiples veces sin error
 -- ============================================================
 
 -- cert2009: 155,322 filas sin ningun indice excepto PRIMARY(id)
 -- El JOIN con rut hace full table scan de 155k x 38k = 6 BILLONES de comparaciones
 
-ALTER TABLE cert2009 ADD INDEX idx_estado (estado);
-ALTER TABLE cert2009 ADD INDEX idx_fecha_solicitud (fecha_solicitud);
-ALTER TABLE cert2009 ADD INDEX idx_rut (rut);
-ALTER TABLE cert2009 ADD INDEX idx_idcert (idcert);
-ALTER TABLE cert2009 ADD INDEX idx_estado_id (estado, id);
-ALTER TABLE cert2009 ADD INDEX idx_estado_fecha (estado, fecha_solicitud);
+CREATE INDEX idx_estado ON cert2009 (estado) ALGORITHM=INPLACE;
+CREATE INDEX idx_fecha_solicitud ON cert2009 (fecha_solicitud) ALGORITHM=INPLACE;
+CREATE INDEX idx_rut ON cert2009 (rut) ALGORITHM=INPLACE;
+CREATE INDEX idx_idcert ON cert2009 (idcert) ALGORITHM=INPLACE;
+CREATE INDEX idx_estado_id ON cert2009 (estado, id) ALGORITHM=INPLACE;
+CREATE INDEX idx_estado_fecha ON cert2009 (estado, fecha_solicitud) ALGORITHM=INPLACE;
+CREATE INDEX idx_rol ON cert2009 (rol) ALGORITHM=INPLACE;
 
 -- rut: 38,511 filas - el campo 'rut' NO tiene indice, solo 'id' (PK)
 -- Todos los JOINs y WHERE buscan por rut.rut, no por rut.id
-ALTER TABLE rut ADD INDEX idx_rut_rut (rut);
-
--- rut: busqueda por rol en consultas
-ALTER TABLE cert2009 ADD INDEX idx_rol (rol);
+CREATE INDEX idx_rut_rut ON rut (rut) ALGORITHM=INPLACE;
 
 -- usuarios: se busca por campo 'usuario' sin indice
-ALTER TABLE usuarios ADD INDEX idx_usuario (usuario);
+CREATE INDEX idx_usuario ON usuarios (usuario) ALGORITHM=INPLACE;
